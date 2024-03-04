19c614e8a714efdf2a0544a96549843e511cb5da
Aramdhann <radityamochamad@gmail.com>
2024-03-04 15:42:01 +0700

    make responsive title in each route page (login and home)

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -3,9 +3,11 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class Login extends Component
 {
+    #[Title('Login')]
     public function render()
     {
         return view('livewire.login');
