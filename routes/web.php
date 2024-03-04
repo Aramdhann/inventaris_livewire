3500556541842b56f7313fe958a799e2f3f29b32
Aramdhann <radityamochamad@gmail.com>
2024-03-04 15:31:26 +0700

    make a route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Livewire\Home::class)->name('home');
+Route::get('/login', \App\Livewire\Login::class)->name('login');
